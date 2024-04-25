3833ce792c18c0e8396a5d2ba20900edff01aa8e
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-04-25 10:32:12 +0300

    completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,14 @@
+// import libraries needed for testing
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
-function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
-}
+// import the component you wrote
+import Article from "../components/Article";
 
-export default Article;
+// test the component
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
-function Header(props) {
-      return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
-}
+import Header from "../components/Header";
 
-export default Header;
+test("displays the text 'hello from the Header!'", () => {
+  render(<Header />);
+
+  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+});
